36a4b70c520e06053dd90d2a6cbf251e05af0089
Safak <eroglusafak24@gmail.com>
2022-03-11 16:51:53 +0300

    commit

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
     let newBook = new book(title, author, pages, read);
     myLibrary.push(newBook);
-    
+    // render();
     }
 
 
@@ -70,68 +70,78 @@ function displayTotalBooks(arr){
 
 
 
-// Render the myLibrary array with rows of books inside of #tbody.
-function render() {
-  let tbody = document.getElementById('tbody');
-  tbody.innerHTML = '';
-  for (let i = 0; i < myLibrary.length; i++) {
-    let tr = document.createElement('tr');
-    // Change tr class for Tailwind CSS.
-    
-    document.getElementById(tr).className = 'bg-white border-b dark:bg-gray-800 dark:border-gray-700';
-    // Create td elements for each book.
-    let tdTitle = document.createElement('td');
-    tdTitle.id = 'book-title';
-    document.getElementById('book-title').className = 'py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white';
-    //Display book title.
-    tdTitle.textContent = myLibrary[i].title;
-    let tdAuthor = document.createElement('td');
-    tdAuthor.id = 'author-pages';
-    //Display book author and pages.
-    tdAuthor.textContent = myLibrary[i].author
-    let tdPages = document.createElement('td');
-    tdPages.id = 'author-pages';
-    tdPages.textContent = myLibrary[i].pages;
-    document.getElementById('author-pages').className = 'py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400';
-    let tdRead = document.createElement('td');
-    tdRead.id = 'book-read-unread';
-    document.getElementById('book-read-unread').className = 'py-4 px-6 text-sm font-medium text-right whitespace-nowrap';
-    // If book is read
-    if (myLibrary[i].read === "read") {
-      // Create a element inside of book-read-unread with text "read" and class "text-green-600 dark:text-green-500 hover:underline"
-      let read = document.createElement('a');
-      read.textContent = "read";
-      read.className = "text-green-600 dark:text-green-500 hover:underline";
+
+
+
+
+
+
+
+
+
+
+// Render the  array with rows of books inside of #tbody.
+// function render(arr) {
+//   let tbody = document.getElementById('tbody');
+//   tbody.innerHTML = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     let tr = document.createElement('tr');
+//     // Change tr class for Tailwind CSS.
+//     tr.id = 'product';
+//     document.getElementById('product').classList.add('bg-white border-b dark:bg-gray-800 dark:border-gray-700');
+//     // Create td elements for each book.
+//     let tdTitle = document.createElement('td');
+//     tdTitle.id = 'book-title';
+//     document.getElementById('book-title').className = 'py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white';
+//     //Display book title.
+//     tdTitle.textContent = arr[i].title;
+//     let tdAuthor = document.createElement('td');
+//     tdAuthor.id = 'author-pages';
+//     //Display book author and pages.
+//     tdAuthor.textContent = arr[i].author
+//     let tdPages = document.createElement('td');
+//     tdPages.id = 'author-pages';
+//     tdPages.textContent = arr[i].pages;
+//     document.getElementById('author-pages').className = 'py-4 px-6 text-sm text-gray-500 whitespace-nowrap dark:text-gray-400';
+//     let tdRead = document.createElement('td');
+//     tdRead.id = 'book-read-unread';
+//     document.getElementById('book-read-unread').className = 'py-4 px-6 text-sm font-medium text-right whitespace-nowrap';
+//     // If book is read
+//     if (arr[i].read === "read") {
+//       // Create a element inside of book-read-unread with text "read" and class "text-green-600 dark:text-green-500 hover:underline"
+//       let read = document.createElement('a');
+//       read.textContent = "read";
+//       read.className = "text-green-600 dark:text-green-500 hover:underline";
       
 
-      // Append the read element to the td element.
-      tdRead.appendChild(read);
-    } else {
-      // Create a element inside of book-read-unread with text "unread" and class "text-red-600 dark:text-red-500 hover:underline"
-      let unread = document.createElement('a');
-      unread.textContent = "unread";
-      unread.className = "text-red-600 dark:text-red-500 hover:underline";
-      // Append the unread element to the td element.
-      tdRead.appendChild(unread);
-    }
-    let tdDelete = document.createElement('td');
-    tdDelete.id = 'delete-book';
-    document.getElementById('delete-book').className = 'py-4 px-6 text-sm font-medium text-right whitespace-nowrap';
-    // Create a element inside of delete-book with text "delete" and class "text-red-600 dark:text-red-500 hover:underline"
-    let deleteBook = document.createElement('a');
-    deleteBook.textContent = "delete";
-    deleteBook.className = "text-red-600 dark:text-red-500 hover:underline";
-    // Append the delete element to the td element.
-    tdDelete.appendChild(deleteBook);
-    // Append the td elements to the tr element.
-    tr.appendChild(tdTitle);
-    tr.appendChild(tdAuthor);
-    tr.appendChild(tdPages);
-    tr.appendChild(tdRead);
-    tr.appendChild(tdDelete);
-    // Append the tr element to the tbody element.
-    tbody.appendChild(tr);
-  }
-}
+//       // Append the read element to the td element.
+//       tdRead.appendChild(read);
+//     } else {
+//       // Create a element inside of book-read-unread with text "unread" and class "text-red-600 dark:text-red-500 hover:underline"
+//       let unread = document.createElement('a');
+//       unread.textContent = "unread";
+//       unread.className = "text-red-600 dark:text-red-500 hover:underline";
+//       // Append the unread element to the td element.
+//       tdRead.appendChild(unread);
+//     }
+//     let tdDelete = document.createElement('td');
+//     tdDelete.id = 'delete-book';
+//     document.getElementById('delete-book').className = 'py-4 px-6 text-sm font-medium text-right whitespace-nowrap';
+//     // Create a element inside of delete-book with text "delete" and class "text-red-600 dark:text-red-500 hover:underline"
+//     let deleteBook = document.createElement('a');
+//     deleteBook.textContent = "delete";
+//     deleteBook.className = "text-red-600 dark:text-red-500 hover:underline";
+//     // Append the delete element to the td element.
+//     tdDelete.appendChild(deleteBook);
+//     // Append the td elements to the tr element.
+//     tr.appendChild(tdTitle);
+//     tr.appendChild(tdAuthor);
+//     tr.appendChild(tdPages);
+//     tr.appendChild(tdRead);
+//     tr.appendChild(tdDelete);
+//     // Append the tr element to the tbody element.
+//     tbody.appendChild(tr);
+//   }
+// }
 
 
